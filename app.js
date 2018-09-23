0e2475fb9684c41e83872c3684ee7a79b6c91ba5
Kix <han880805@gmail.com>
2018-09-23 16:21:29 +0800

    updated 20180923

--- app.js
+++ app.js
@@ -1,105 +1,55 @@
-const phantom = require('phantom');
+(async () => {
+    const phantom = require('phantom'), browser = await phantom.create(), page = await browser.createPage(), course = require('./course.json'),
+        numbers = [
+            "011110100001100001101101101101101101101101100001100001011110",
+            "001000111000001000001000001000001000001000001000001000111110",
+            "011110100001100001000001000010000100001000010000100001111111",
+            "011110100001100001000010001100000010000001100001100001011110",
+            "000100000100001100010100100100100100111111000100000100001111",
+            "111111100000100000101110110001000001000001100001100001011110",
+            "001110010001100000100000101110110001100001100001100001011110",
+            "111111100010100010000100000100001000001000001000001000001000",
+            "011110100001100001100001011110010010100001100001100001011110",
+            "011100100010100001100001100011011101000001000001100010011100"
+        ],
+        pData = [
+            "Account=" + process.argv[2] + "&Password=" + process.argv[3] + "&B1=%B5n%A1%40%A1%40%A4J",
+            "&agree=agree&" + "Cono1=" + course.Cono1 + "&Coname1=" + course.Coname1 + "&LKind1=" + course.LKind1 + "&Yclass1=" + course.Yclass1 + "&TWeek1=" + course.TWeek1 + "&TCard1=" + course.TCard1 + "&Num1=" + course.Num1 + "&Notup1=False&Limit1=False&Limit21=1&" + "Cono2=" + course.Cono2 + "&Coname2=" + course.Coname2 + "&LKind2=" + course.LKind2 + "&Yclass2=" + course.Yclass2 + "&TWeek2=" + course.TWeek2 + "&TCard2=" + course.TCard2 + "&Num2=" + course.Num2 + "&Notup2=False&Limit2=False&Limit22=1&" + "Cono3=" + course.Cono3 + "&Coname3=" + course.Coname3 + "&LKind3=" + course.LKind3 + "&Yclass3=" + course.Yclass3 + "&TWeek3=" + course.TWeek3 + "&TCard3=" + course.TCard3 + "&Num3=" + course.Num3 + "&Notup3=False&Limit3=False&Limit23=1&" + "Cono4=" + course.Cono4 + "&Coname4=" + course.Coname4 + "&LKind4=" + course.LKind4 + "&Yclass4=" + course.Yclass4 + "&TWeek4=" + course.TWeek4 + "&TCard4=" + course.TCard4 + "&Num4=" + course.Num4 + "&Notup4=False&Limit4=False&Limit24=1&" + "Cono5=" + course.Cono5 + "&Coname5=" + course.Coname5 + "&LKind5=" + course.LKind5 + "&Yclass5=" + course.Yclass5 + "&TWeek5=" + course.TWeek5 + "&TCard5=" + course.TCard5 + "&Num5=" + course.Num5 + "&Notup5=False&Limit5=False&Limit25=1&" + "Cono6=" + course.Cono6 + "&Coname6=" + course.Coname6 + "&LKind6=" + course.LKind6 + "&Yclass6=" + course.Yclass6 + "&TWeek6=" + course.TWeek6 + "&TCard6=" + course.TCard6 + "&Num6=" + course.Num6 + "&Notup6=False&Limit6=False&Limit26=1&" + "Cono7=" + course.Cono7 + "&Coname7=" + course.Coname7 + "&LKind7=" + course.LKind7 + "&Yclass7=" + course.Yclass7 + "&TWeek7=" + course.TWeek7 + "&TCard7=" + course.TCard7 + "&Num7=" + course.Num7 + "&Notup7=False&Limit7=False&Limit27=1&" + "Cono8=" + course.Cono8 + "&Coname8=" + course.Coname8 + "&LKind8=" + course.LKind8 + "&Yclass8=" + course.Yclass8 + "&TWeek8=" + course.TWeek8 + "&TCard8=" + course.TCard8 + "&Num8=" + course.Num8 + "&Notup8=False&Limit8=False&Limit28=1&" + "Cono9=" + course.Cono9 + "&Coname9=" + course.Coname9 + "&LKind9=" + course.LKind9 + "&Yclass9=" + course.Yclass9 + "&TWeek9=" + course.TWeek9 + "&TCard9=" + course.TCard9 + "&Num9=" + course.Num9 + "&Notup9=False&Limit9=False&Limit29=1&" + "Cono10=" + course.Cono10 + "&Coname10=" + course.Coname10 + "&LKind10=" + course.LKind10 + "&Yclass10=" + course.Yclass10 + "&TWeek10=" + course.TWeek10 + "&TCard10=" + course.TCard10 + "&Num10=" + course.Num10 + "&Notup10=False&Limit10=False&Limit210=1"
+        ];
 
-function delay(sec) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve('Success');
-        }, sec * 1000);
-    });
-}
-
-(async function () {
-    const browser = await phantom.create();
-    const page = await browser.createPage();
-    await page.on("onResourceRequested", function (requestData) {
+    page.on("onResourceRequested", (requestData) => {
         console.info('Requesting', requestData.url)
     });
-    const status = await page.open('https://course.nuk.edu.tw/Sel/login.asp');
-    console.log(status);
-    var content = await page.property('url');
-    console.log(content);
-    var script = await page.evaluate(
-        function (username,password) {
-            document.querySelectorAll("input")[1].value = username;
-            document.querySelectorAll("input")[3].value = password;
-            document.querySelector("form").submit();
-        },process.argv[2],process.argv[3]);
-    await delay(0.5).then(async function () {
-        content = await page.property('url');
-        console.log(content);
-        page.evaluate(function () { document.querySelectorAll("a")[3].click(); });
-        await delay(0.5).then(async function () {
-            content = await page.property('url');
-            console.log(content);
-            var swit = true;
-            while(swit == true) {page.evaluate(function () { document.querySelector("[name = header]").contentDocument.querySelector('[value = "加選"]').click(); });
-            await delay(0.5).then(async function () {
-                content = await page.property('url');
-                console.log(content);
-                page.evaluate(
-                    function () {
-                        selectpage = document.querySelector("[name = main]").contentDocument.querySelector('[name = main2]').contentDocument;
-                        var form = selectpage.Hidden_Form;
-                        form.cho_cono.value = "CCO413";
-                        form.cho_name.value = "";
-                        form.cho_lkind.value = "2";
-                        form.cho_yclass.value = "A10707";
-                        form.cho_week.value = "1,1,1";
-                        form.cho_card.value = "7,8,9";
-                        form.cho_num.value = "3";
-                        form.cho_notup.value = "False";
-                        form.cho_limit.value = "False";
-                        form.cho_limit2.value = "1";
-                        selectpage.Course_Form.agree.checked = true;
-                        selectpage.Course_Form.B1.disabled = false;
-                        for (var i = 0; i <= 0; i++) selectpage.Course_Form.querySelectorAll("table tbody tr td [name ^= Cono]")[i].click();
-                    });
-                await delay(0.5).then(async function () {
-                    page.evaluate(function () {
-                        selectpage = document.querySelector("[name = main]").contentDocument.querySelector('[name = main2]').contentDocument;
-                        function AutoCaptcha() {
-                            var image = selectpage.Course_Form.Certify_Image;
-                            var canvas = selectpage.createElement('canvas');
-                            var ctx = canvas.getContext("2d");
-                            var numbers = [
-                                "011110100001100001101101101101101101101101100001100001011110", "001000111000001000001000001000001000001000001000001000111110", "011110100001100001000001000010000100001000010000100001111111", "011110100001100001000010001100000010000001100001100001011110", "000100000100001100010100100100100100111111000100000100001111", "111111100000100000101110110001000001000001100001100001011110", "001110010001100000100000101110110001100001100001100001011110", "111111100010100010000100000100001000001000001000001000001000", "011110100001100001100001011110010010100001100001100001011110", "011100100010100001100001100011011101000001000001100010011100"
-                            ];
-                            var captcha = "";
-                            canvas.width = image.width;
-                            canvas.height = image.height;
-                            canvas.hidden = true;
-                            selectpage.body.appendChild(canvas);
-                            ctx.drawImage(image, 0, 0);
-                            for (var i = 0; i < 4; i++) {
-                                var pixels = ctx.getImageData(10 * i + 2, 0, 6, 10).data;
-                                var ldString = "";
-                                for (var j = 0, length = pixels.length; j < length; j += 4) {
-                                    ldString = ldString + ((pixels[j] == 255) ? 0 : 1);
-                                }
-                                for (var k = 0; k < 10; k++) {
-                                    if (ldString == numbers[k]) captcha += k.toString();
-                                }
-                            }
-                            selectpage.Course_Form.Certify.value = captcha;
-                        }
-                        AutoCaptcha();
-                    })
-                    await delay(0.5).then(async function () {
-                    page.evaluate(function (){document.querySelector("[name = main]").contentDocument.querySelector('[name = main2]').contentWindow.validate();});
-                        await delay(0.5).then(async function () {
-                            content = await page.evaluate(function(){
-                                return document.querySelector("[name = main]").contentDocument.querySelector("p").innerText;
-                            });
-                            console.log("選課結果"+content);
-                            if (!content.search("失敗")) {
-                                swit = false
-                                browser.exit();
-                                return;
-                            }
-                        })
-                    })
-                })
-            })}
-        })
-    })
-}());
+
+    while (true) {
+        await page.open('https://course.nuk.edu.tw/Sel/SelectMain1.asp', 'POST', pData[0]);
+        await page.open('https://course.nuk.edu.tw/Sel/Main0195.asp', 'POST', "Prgid=CO0200");
+        await page.open("https://course.nuk.edu.tw/Sel/Certify_Image.asp");
+        let ctext = await page.evaluate(function (numbers) {
+            var image = document.querySelector("img");
+            var canvas = document.createElement('canvas');
+            var ctx = canvas.getContext("2d");
+            var captcha = "";
+            canvas.width = image.width;
+            canvas.height = image.height;
+            ctx.drawImage(image, 0, 0);
+            for (var i = 0; i < 4; i++) {
+                var dots = ctx.getImageData(10 * i + 2, 0, 6, 10).data;
+                var isBlack = "";
+                for (var j = 0, length = dots.length; j < length; j += 4) {
+                    isBlack = isBlack + ((dots[j] == 255) ? 0 : 1);
+                }
+                for (var k = 0; k < 10; k++) {
+                    if (isBlack == numbers[k]) captcha += k.toString();
+                }
+            }
+            return captcha;
+        }, numbers);
+        await page.open('https://course.nuk.edu.tw/Sel/AddScript.asp', 'POST', "total_count=10&Beg_Time=&Certify=" + ctext + pData[1]);
+        let plain = await page.property('plainText');
+        console.log(plain);
+        if ((plain.search("成功") > -1) && (plain.search("錯誤") == -1)) break;
+    }
+    browser.exit();
+    return;
+})();
